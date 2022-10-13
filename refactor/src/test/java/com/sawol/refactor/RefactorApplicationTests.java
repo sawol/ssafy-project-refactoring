c0e96800e6a7f04927de8d5c818612d3ec4776c1
sawol <sawol634@gmail.com>
2022-10-13 23:40:37 +0900

    초기 셋팅

---
+++ refactor/src/test/java/com/sawol/refactor/RefactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sawol.refactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
