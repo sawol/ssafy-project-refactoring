8e985aab8331d6ccdb968a214459cf18312fcd83
sawol <sawol634@gmail.com>
2022-10-26 00:05:19 +0900

    직접 만든 세션 테스트

---
+++ refactor/src/test/java/com/sawol/refactor/web/session/SessionManagerTest.java
@@ -0,0 +1,34 @@
+package com.sawol.refactor.web.session;
+
+import com.sawol.refactor.domain.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+class SessionManagerTest {
+
+    SessionManager sessionManager = new SessionManager();
+
+    @Test
+    void sessionTest() {
+
+        // 세션 생성
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        Member member = new Member();
+        sessionManager.createSession(member, response);
+
+        // 요청에 응답 쿠키 저장
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setCookies(response.getCookies());
+
+        // 세션 조회
+        Object result = sessionManager.getSession(request);
+        Assertions.assertThat(result).isEqualTo(member);
+
+        // 세션 만료
+        sessionManager.expire(request);
+        Object expiredSession = sessionManager.getSession(request);
+        Assertions.assertThat(expiredSession).isNull();
+    }
+}
